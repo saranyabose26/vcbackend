ab4c1e9bf939a4b65405d8c1785e9924b20ed1db
saranya <147645957+saranyabose26@users.noreply.github.com>
2024-09-19 23:20:42 +0530

    mail.js

--- MailUtils/mail.js
+++ MailUtils/mail.js
@@ -8,14 +8,14 @@ dotenv.config();
 const transport = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: "nithishkumarmurugesan2001@gmail.com",
+        user: "sarantwo266@gmail.com",
         pass: process.env.MAIL_PASSWORD
     },
 });
 
 //Mail content
 const mailOption = {
-    from: "nithishkumarmurugesan2001@gmail.com",
+    from:  "sarantwo266@gmail.com",
     to: [],
     subject: "Appointment date For Your Vehicle",
     text: "Today is your vehicle Appointment date. So provide your vehicle to the service center. Important note I have used node-schedule package to schedule task on specific date but it work in local but not work in render.com",
